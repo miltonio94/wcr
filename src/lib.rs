8adba971e747824c57778e6858528f168c1ef6b2
Milton <miltonio94@gmail.com>
2023-08-02 09:03:00 +0000

    get cli args

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
+use clap::{App, Arg};
 use std::error::Error;
 
 type MyResult<R> = Result<R, Box<dyn Error>>;
 
-struct Config {
+#[derive(Debug)]
+pub struct Config {
     files: Vec<String>,
     lines: bool,
     words: bool,
@@ -10,8 +12,43 @@ struct Config {
     chars: bool,
 }
 
-fn get_args() -> MyResult<Config> {
-    //
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("wcr")
+        .version("0.1.0")
+        .author("M")
+        .about("Rust wc")
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILE")
+                .help("Input file(s)")
+                .multiple(true)
+                .default_value("-"),
+        )
+        .arg(
+            Arg::with_name("lines")
+                .short("l")
+                .long("lines")
+                .value_name("LINES"),
+        )
+        .arg(
+            Arg::with_name("words")
+                .short("w")
+                .long("words")
+                .value_name("WORDS"),
+        )
+        .arg(
+            Arg::with_name("bytes")
+                .short("c")
+                .long("bytes")
+                .value_name("BYTES"),
+        )
+        .arg(
+            Arg::with_name("chars")
+                .short("m")
+                .long("chars")
+                .value_name("CHARS"),
+        )
+        .get_matches();
     Ok(Config {
         files: vec!["kasfjl".to_string()],
         lines: true,
@@ -21,7 +58,7 @@ fn get_args() -> MyResult<Config> {
     })
 }
 
-fn run(config: Config) -> MyResult<()> {
+pub fn run(config: Config) -> MyResult<()> {
     //
     Ok(())
 }
